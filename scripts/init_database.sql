18783f53eea9ddecd7764f313c36e515bae8571e
Juan Carlos Tapia <juancarlostapiab.1@gmail.com>
2025-05-21 15:18:50 -0600

    Updated init_database script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -21,7 +21,7 @@ GO
 -- Drop and recreate DataWarehouse database
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK INMEDIATE;
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DataWarehouse
 END;
 GO
@@ -35,7 +35,7 @@ GO
 
 -- Create Schemas
 CREATE SCHEMA bronze;
-GO;
+GO
 
 CREATE SCHEMA silver;
 GO
